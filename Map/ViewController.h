b0e0140885cab5dc3e40a2a263e0440410575d17
Martin Kautz <kautz@jakota.de>
2015-06-16 10:35:48 +0200

    Initial Commit

---
+++ Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map
+//
+//  Created by Martin Kautz on 16.06.15.
+//  Copyright (c) 2015 JAKOTA Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Map/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Map
+//
+//  Created by Martin Kautz on 16.06.15.
+//  Copyright (c) 2015 JAKOTA Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
