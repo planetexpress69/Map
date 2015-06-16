8a6f110cc853b8f25fa380cffb0abdd67a4c4d89
Martin Kautz <kautz@jakota.de>
2015-06-16 11:03:40 +0200

    Added code to intercept a long press.

--- Map/ViewController.m
+++ Map/ViewController.m
@@ -19,18 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILongPressGestureRecognizer *longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(addPin:)];
+    longPressGestureRecognizer.minimumPressDuration = .3f;
+    [_theMapView addGestureRecognizer:longPressGestureRecognizer];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
 
     [super viewDidAppear:animated];
     CLLocationCoordinate2D initialCenterCoordinate = CLLocationCoordinate2DMake(52.5, 13.4);
-    [self.theMapView setCenterCoordinate:initialCenterCoordinate zoomLevel:10 animated:YES];
+    [_theMapView setCenterCoordinate:initialCenterCoordinate zoomLevel:10 animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addPin:(id)sender {
+    UILongPressGestureRecognizer *longPressGestureRecognizer = (UILongPressGestureRecognizer *)sender;
+
+    if (longPressGestureRecognizer.state != UIGestureRecognizerStateBegan)
+        return;
+
+    CGPoint pointInView = [longPressGestureRecognizer locationInView:_theMapView];
+    CLLocationCoordinate2D coord = [_theMapView convertPoint:pointInView toCoordinateFromView:_theMapView];
+    NSLog(@"lat: %.2f / %.2f", coord.latitude, coord.longitude);
 }
 
 @end
